0b6fd5c9397b3173fbc51dc111fbc107db8e81a4
Paul Diamond <pfdiamond13@gmail.com>
2020-02-10 16:55:29 -0800

    Add basic Skeleton

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,72 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-const App = () => {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+interface Props {
+}
+
+interface State {
+  dmgTotal: number
+  listOfCardsPlayed: {name: string, dmg: number}[]
+}
+class App extends React.Component< Props, State > {
+  constructor(props: Props) {
+    super(props);
+    this.state = { dmgTotal: 0, listOfCardsPlayed: [{ name: 'Test', dmg: 5 }] };
+  }
+
+  calculateDamge = () => {
+    let basicDamage = this.state.listOfCardsPlayed.reduce((acc, obj) => (acc + obj.dmg), 0);
+    this.setState({ dmgTotal: basicDamage });
+  }
+
+  render() {
+    const { dmgTotal, listOfCardsPlayed } = this.state;
+    const cardsPlayed = listOfCardsPlayed.map((card) => <li>Name:{card.name} Damage: {card.dmg}</li>);
+    return (
+      <div className="App">
+        <div>Ironclad</div>
+        <div>
+          Damage Total:
+          {dmgTotal}
+        </div>
+        <div>Add Card:</div>
+        <div>Cards Played:</div>
+        <ul>{cardsPlayed}</ul>
+        <button onClick={this.calculateDamge}>Test</button>
+      </div>
+    );
+  }
 }
 
 export default App;
+
+// import React from 'react';
+
+// interface Props {
+//   currentChannel: string;
+//   height: string;
+//   width: string;
+//   frameBorder: string;
+//   scrolling: string;
+//   allowFullScreen: boolean;
+// }
+// const TwitchPlayer: React.FC<Props> = ({
+//   currentChannel, height, width, frameBorder, scrolling, allowFullScreen,
+// }) => {
+//   return (
+//     <div className="App">
+//       <iframe
+//         title="twitchPlayer"
+//         src={`https://player.twitch.tv/?channel=${currentChannel}`}
+//         height={height}
+//         width={width}
+//         frameBorder={frameBorder}
+//         scrolling={scrolling}
+//         allowFullScreen={allowFullScreen}
+//       />
+//      </div>
+//   );
+// };
+
+
+// export default TwitchPlayer;
